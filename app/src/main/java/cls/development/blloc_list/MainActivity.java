2dad66641207e77f265e50f09b29f4d2cbf21ce9
Caleb <primocaleb@gmail.com>
2020-09-04 19:01:30 +0200

    finished some beta stuff

--- app/src/main/java/cls/development/blloc_list/MainActivity.java
+++ app/src/main/java/cls/development/blloc_list/MainActivity.java
@@ -9,6 +9,7 @@ import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Build;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.MotionEvent;
 import android.view.View;
 import android.widget.LinearLayout;
@@ -21,9 +22,14 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        MotionLayout motionLayout = findViewById(R.id.motionlayout_1);
+        final MotionLayout motionLayout = findViewById(R.id.motionlayout_1);
         //motionLayout.getTransition(R.id.motionlayout_1).setEnable(false);
         LinearLayout linearLayout = findViewById(R.id.linear);
+
+
+
+
+
 
 
         Toast.makeText(this, "enabled: "+motionLayout.isInteractionEnabled(), Toast.LENGTH_SHORT).show();
@@ -33,9 +39,49 @@ public class MainActivity extends AppCompatActivity {
 
 
 
-        RecyclerView recyclerView2 = findViewById(R.id.rec_2);
+        final RecyclerView recyclerView2 = findViewById(R.id.rec_2);
+
         recyclerView2.setLayoutManager(new LinearLayoutManager(this));
         recyclerView2.setAdapter(new recyclerViewAdapter());
+        recyclerView2.setOnScrollChangeListener(new View.OnScrollChangeListener() {
+            @Override
+            public void onScrollChange(View view, int i, int i1, int i2, int i3) {
+                boolean state =motionLayout.isActivated();
+                Log.d("State23","state: " + state);
+                motionLayout.setTransitionListener(new MotionLayout.TransitionListener() {
+                    @Override
+                    public void onTransitionStarted(MotionLayout motionLayout, int i, int i1) {
+
+                    }
+
+                    @Override
+                    public void onTransitionChange(MotionLayout motionLayout, int i, int i1, float v) {
+
+                    }
+
+                    @Override
+                    public void onTransitionCompleted(MotionLayout motionLayout, int i) {
+                        recyclerView2.setLayoutFrozen(false);
+
+                    }
+
+                    @Override
+                    public void onTransitionTrigger(MotionLayout motionLayout, int i, boolean b, float v) {
+                        recyclerView2.setLayoutFrozen(true);
+
+
+
+                    }
+                });
+
+
+            }
+        });
+
+
+
+
+
 
 
 
